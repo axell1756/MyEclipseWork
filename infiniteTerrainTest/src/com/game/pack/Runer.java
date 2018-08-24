a77602c69da29ea7bf07284e69aa69f2858384ae
axell1756 <gniteckis@gmail.com>
2018-08-24 13:13:54 +0100

    infiniteTerain Java Test

--- infiniteTerrainTest/src/com/game/pack/Runer.java
+++ infiniteTerrainTest/src/com/game/pack/Runer.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Runer {
 
-	public static Treasure t = new Treasure(4, 1);
+	public static Treasure t = new Treasure(4, 0);
 	public static Player p = new Player(0, 0);
 	
 	public static void main(String[] args) {
@@ -19,10 +19,10 @@ public class Runer {
 
 		do {
 			
-			int playerSum = Math.abs(p.getPosX()) + Math.abs(p.getPosY());
-			int treasureSum = Math.abs(t.getPosX()) + Math.abs(t.getPosY());
-			int distance = Math.abs(playerSum - treasureSum);
-			int distanceAfterStep = distance;
+			int xDifference = Math.abs(Math.abs(p.getPosX()) - Math.abs(t.getPosX()));
+			int yDifference = Math.abs(Math.abs(p.getPosY()) - Math.abs(t.getPosY()));
+			
+			int distanceBetween = Math.abs((Math.abs(p.getPosX()) + Math.abs(p.getPosY())) - (Math.abs(t.getPosX()) + Math.abs(t.getPosY()))) - 1;
 			
 			System.out.print("Which way are you going?: ");
 			String playerInput = sc.nextLine();
@@ -31,29 +31,29 @@ public class Runer {
 			
 			case "n": 
 				p.setPosX(p.getPosX()+1);
-				distanceAfterStep++;
-				System.out.println("Current distance: " + String.valueOf((distanceAfterStep)));
+				System.out.println("Current distance X: " + String.valueOf((xDifference)));
+				System.out.println("Current distance Y: " + String.valueOf((yDifference)));
 				break;
 				
 			case "e":
 				p.setPosX(p.getPosX()-1);
-				distanceAfterStep--;
-				System.out.println("Current distance: " + String.valueOf((distanceAfterStep)));
+				System.out.println("Current distance X: " + String.valueOf((xDifference)));
+				System.out.println("Current distance Y: " + String.valueOf((yDifference)));
 				break;
 				
 			case "w":
 				p.setPosY(p.getPosY()+1);
-				distanceAfterStep++;
-				System.out.println("Current distance: " + String.valueOf((distanceAfterStep)));
+				System.out.println("Current distance X: " + String.valueOf((xDifference)));
+				System.out.println("Current distance Y: " + String.valueOf((yDifference)));
 				break;
 				
 			case "s":
 				p.setPosY(p.getPosY()-1);
-				distanceAfterStep--;
-				System.out.println("Current distance: " + String.valueOf((distanceAfterStep)));
+				System.out.println("Current distance X: " + String.valueOf((xDifference)));
+				System.out.println("Current distance Y: " + String.valueOf((yDifference)));
 				break;
 			}
-			
+			/*
 			if (distance > distanceAfterStep) {
 				System.out.println("You are getting closer!");
 				continue;
@@ -64,7 +64,7 @@ public class Runer {
 				System.out.println("You are getting further!");
 				continue;
 			}
-
+			 */
 		} while ((t.getPosX() != p.getPosX() || t.getPosY() != p.getPosY()) == true);
 
 		sc.close();
